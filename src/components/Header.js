4174f5ce121f701f28786da8172b0e1fff6086f4
Nick <nickmagi24@gmail.com>
2021-11-10 12:53:21 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Header() {
+    return (
+        <div>
+        <h3>Stuff</h3>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    componentWillMount() {
+        
+    }
+    
+    render() {
+        return(
+            <div></div>
+        )
+    }
+    
+}
+
+export default MemeGenerator;
