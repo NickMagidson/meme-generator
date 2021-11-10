2d9dfd204651747fa34469870238fcb1f2aa281d
Nick <nickmagi24@gmail.com>
2021-11-10 13:07:55 -0500

    API fetch

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,11 +3,23 @@ import React from 'react';
 class MemeGenerator extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
     }
     
-    componentWillMount() {
-        
+    componentDidMount() {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data;
+                console.log(memes[0]);
+                this.setState({allMemeImgs: memes})
+            })
+
     }
     
     render() {
